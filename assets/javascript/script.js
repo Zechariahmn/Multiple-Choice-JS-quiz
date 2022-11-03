0934b8232906854289e13dd68bff8b6803815e2f
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-11-03 16:45:21 -0500

    added question elements into script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,4 +2,14 @@ let viewHighScores = document.getElementById('scores');
 let timer = document.getElementById('time');
 let seconds = document.getElementById('seconds');
 let startPage = document.getElementById('begin');
-let startBtn = document.getElementById('start');
+let startBtn = document.getElementById('START');
+
+let questions = document.querySelectorAll('.question');
+let optionsOne = document.getElementById('one');
+let optionsTwo = document.getElementById('two');
+let optionsThree = document.getElementById('three');
+let optionsFour = document.getElementById('four');
+let optionsFive = document.getElementById('five');
+let optionsix = document.getElementById('six');
+let optionsSeven = document.getElementById('seven');
+
