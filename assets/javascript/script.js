756b65b513a395a32810051ab0862d0c6a4b9a28
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-11-03 16:41:43 -0500

    added creation of elements js for start page

---
+++ assets/javascript/script.js
@@ -0,0 +1,5 @@
+let viewHighScores = document.getElementById('scores');
+let timer = document.getElementById('time');
+let seconds = document.getElementById('seconds');
+let startPage = document.getElementById('begin');
+let startBtn = document.getElementById('start');
